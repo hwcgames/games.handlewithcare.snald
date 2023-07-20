8c7c6ca445535281f55ab0f823a83f86ab567370
Willow <willow.ch@protonmail.com>
2023-07-20 17:59:04 -0500

    redo the whole thing

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-flatpak install org.godotengine.godot.BaseApp/x86_64/3.4
-flatpak-builder --repo=./repo --force-clean $@ ./build com.hwcgames.snald.json
+flatpak install org.godotengine.godot.BaseApp/x86_64/3.5
+flatpak-builder --repo=./repo --force-clean $@ ./build games.handlewithcare.snald.yml
--- entry.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /app/bin
-./godot-runner
